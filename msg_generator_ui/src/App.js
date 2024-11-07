c2eedf6649de1eec46b88f087b88c8f1b2c746a2
Zainab Firdous <zainabfirdous786@gmail.com>
2024-11-07 17:00:46 +0530

    UI for displaying message and posts

--- msg_generator_ui/src/App.js
+++ msg_generator_ui/src/App.js
@@ -1,24 +1,47 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState } from 'react';
+import axios from 'axios'; 
 
 function App() {
   //using state to create payload for api call
   const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    message: '',
+    username: '',
+    password: '',
+    profileUrl: '',
   });
+
+  const [message, setMessage] = useState('');
+
+  const [user, setUser] = useState({
+    name: '',
+    location: '',
+    posts: []
+  })
 
   //to capture changes in the form-input
   const handleChange = (e)=>{
     const {name, value} = e.target;
+    console.log(formData)
     setFormData({...formData, [name]:value});
   }
 
   //call backend api using formData to generate personalised message
   const handleSubmit = async(e)=>{
     e.preventDefault();
+    const user_data = await axios.post('http://127.0.0.1:8000/', formData)
+    setUser({
+      name:user_data.data.details.name,
+      location:user_data.data.details.location,
+      posts:user_data.data.details.posts
+    })
+  }
+
+  const generateMessage = async(e)=>{
+    e.preventDefault()
+    let result = await axios.get(`http://127.0.0.1:8000/generate?name=${user.name}`)
+    console.log(result)
+    setMessage(result.data[0])
   }
 
   return (
@@ -39,13 +62,35 @@ function App() {
         </div>
         <div class = "row">
           <div class = "col"><label class = "form-label">Target Profile URL</label></div>
-          <div class = "col"> <input name="profileurl" class = "form-control text-center" placeholder = "Profile URL" value={FormData.password} onChange={handleChange}/></div>       
+          <div class = "col"> <input name="profileUrl" class = "form-control text-center" placeholder = "Profile URL" value={FormData.profileUrl} onChange={handleChange}/></div>       
         </div>
         <div class = "col">
-          <button type="submit" class="btn btn-success btn-md">Generate Message</button>
+          <button type="submit" class="btn btn-success btn-md">Fetch Posts</button>
         </div>
         </form>
       </div>
+
+      {user.posts.length>0 && message === '' && 
+      <div class="container text-center">
+      <button type='submit' class="btn btn-success btn-md" onClick={generateMessage}>Generate Message</button>
+      <table class="table">
+        <thead>
+          <tr><th scope='col'>Posts</th></tr>
+        </thead>
+        <tbody>
+          {user.posts.map((post,index)=>(
+            <tr key={index}>
+              <td>{post}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      </div> }
+
+      {message.length>0 &&
+      <div class="container text-center">
+        {message}
+      </div>}
     </div>
   );
 }
